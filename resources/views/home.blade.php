e4bbd8a15ed3b153ecb8da4cba090e0573fd5158
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-07-29 12:12:25 +0200

    Create card che mostra tutti i treni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,24 @@
 @extends('layout.app')
 
-@section('content')
+@section('content') 
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                
-            </div>
+        <div class="row mt-5">
+            <h1 class="mb-3">Trains</h1>
+            @foreach($trains as $train)
+                <div class="col-4">
+                    <div class="card p-3 mb-3">
+                        <h3>Azienda: <strong>{{ $train->azienda }}</strong></h3>
+                        <p>Stazione di partenza: <strong>{{ $train->stazione_di_partenza }}</strong></p>
+                        <p>Stazione di arrivo: <strong>{{ $train->stazione_di_arrivo }}</strong></p>
+                        <p>Orario partenza: <strong>{{ $train->orario_partenza }}</strong></p>
+                        <p>Orario arrivo: <strong>{{ $train->orario_arrivo }}</strong></p>
+                        <p>Codice treno: <strong>{{ $train->codice_treno }}</strong></p>
+                        <p>Numero carrozze: <strong>{{ $train->numero_carrozze }}</strong></p>
+                        <p>In orario: <strong>{{ $train->in_orario ? 'Si' : 'No' }}</strong></p>
+                        <p>Cancellazione: <strong>{{ $train->cancellato ? 'Si' : 'No' }}</strong></p>
+                    </div>
+                </div>
+            @endforeach
         </div>
 
     </div>

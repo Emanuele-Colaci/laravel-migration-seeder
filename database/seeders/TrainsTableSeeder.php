2a0313144623dd6453ef8bd8acdd55719a283d9d
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-07-29 18:31:29 +0200

    Popolato seeder aggiunto in db

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -15,8 +15,23 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<10; $i++){
+            $train = new Train();
+
+            $train->azienda = $faker->word();
+            $train->stazione_di_partenza = $faker->city();
+            $train->stazione_di_arrivo = $faker->city();
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->numberBetween(100, 1264);
+            $train->numero_carrozze = $faker->numberBetween(6, 20);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->data_partenza = $faker->date();
+
+            $train->save();
+        };
     }
 }
